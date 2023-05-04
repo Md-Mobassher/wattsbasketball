35e89fc44fbadfff5f31962545099c88268bf6c1
Md-Mobassher <you@example.com>
2023-05-04 10:59:27 +0600

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Home from './Pages/Home/Home';
+import Footer from './Shared/Footer';
+import Navbar from './Shared/Navbar';
 
 function App() {
   return (
     <div >
+      <Navbar />
       <Home />
-
+      <Footer />
     </div>
   );
 }
---
+++ src/Shared/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BsBoxArrowUpRight } from "react-icons/bs";
+
+import logo from "../Assets/logo.png";
+
+const Footer = () => {
+  return (
+    <footer className="pb-20 px-14 bg-black">
+      <div className="flex justify-evenly items-center">
+        <div className="text-white hover:scale-125 overflow-hidden  transition-all text-center w-auto">
+          <BsBoxArrowUpRight className="mx-auto  w-8 h-8" />
+          <p className="mt-3 hover:scale-100">Stay in touch</p>
+        </div>
+
+        <div className="text-white hover:scale-125 overflow-hidden  transition-all text-center w-auto">
+          <BsBoxArrowUpRight className="mx-auto  w-8 h-8" />
+          <p className="mt-3 hover:scale-100">Be Social</p>
+        </div>
+
+        <div className="text-white hover:scale-125 overflow-hidden  transition-all  text-center w-auto">
+          <img className="w-28" src={logo} alt="" />
+        </div>
+
+        <div className="text-white hover:scale-125 overflow-hidden  transition-all text-center w-auto">
+          <BsBoxArrowUpRight className="mx-auto  w-8 h-8" />
+          <p className="mt-3 hover:scale-100">Get ans</p>
+        </div>
+        <div className="text-white hover:scale-125 overflow-hidden  transition-all text-center w-auto">
+          <BsBoxArrowUpRight className="mx-auto  w-8 h-8" />
+          <p className="mt-3 hover:scale-100">Contact Us</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
