afce6d8a91c8de204a67dc9527bf090f5038647d
Md-Mobassher <you@example.com>
2023-05-16 13:45:07 +0600

    footer icon added

--- src/Shared/Footer.js
+++ src/Shared/Footer.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { BsBoxArrowUpRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { BsFillQuestionCircleFill } from "react-icons/bs";
+import { TiContacts } from "react-icons/ti";
+import { AiFillInstagram, AiTwotoneSnippets } from "react-icons/ai";
 
 import logo from "../Assets/logo.png";
-import { Link } from "react-router-dom";
+
 
 const Footer = () => {
   return (
     <footer className="lg:py-10 py-5 bg-black">
       <div className="flex justify-evenly items-center">
         <Link className="text-white hover:scale-125 overflow-hidden  transition-all text-center w-auto">
-          <BsBoxArrowUpRight className="mx-auto  w-8 h-8" />
+          <AiTwotoneSnippets className="mx-auto  w-8 h-8" />
           <p className="mt-3 hover:scale-100  hidden lg:block">Stay in touch</p>
         </Link>
 
         <Link className="text-white hover:scale-125 overflow-hidden  transition-all text-center w-auto">
-          <BsBoxArrowUpRight className="mx-auto  w-8 h-8" />
+          <AiFillInstagram className="mx-auto  w-8 h-8" />
           <p className="mt-3 hover:scale-100 hidden lg:block">Be Social</p>
         </Link>
 
@@ -23,11 +26,11 @@ const Footer = () => {
         </Link>
 
         <Link className="text-white hover:scale-125 overflow-hidden  transition-all text-center w-auto">
-          <BsBoxArrowUpRight className="mx-auto  w-8 h-8" />
+          <BsFillQuestionCircleFill className="mx-auto  w-8 h-8" />
           <p className="mt-3 hover:scale-100  hidden lg:block">Get ans</p>
         </Link>
         <Link className="text-white hover:scale-125 overflow-hidden  transition-all text-center w-auto">
-          <BsBoxArrowUpRight className="mx-auto  w-8 h-8" />
+          <TiContacts className="mx-auto  w-8 h-8" />
           <p className="mt-3 hover:scale-100  hidden lg:block hover:block">Contact Us</p>
         </Link>
       </div>
